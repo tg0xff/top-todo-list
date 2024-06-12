816dcf079859ca8b0423c962a815d517834b8bc4
tg0xff <tg0xff@proton.me>
2024-06-12 16:51:22 -0600

    Turn priority into an accessor property

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 class Todo {
+  #priority = 1;
   constructor(title) {
     this.title = title;
     this.description = "";
     this.dueDate = "";
-    this.priority = 1;
     this.done = false;
     this.nested = [];
+  }
+  set priority(lvl) {
+    if (lvl >= 0 && lvl <= 2) {
+      this.#priority = lvl;
+    }
+  }
+  get priority() {
+    return this.#priority;
   }
   isProject() {
     return this.nested.length > 0;
