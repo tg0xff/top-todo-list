b2505f0bc2449b4b535a4cb1d016787a58f8b7bb
tg0xff <tg0xff@proton.me>
2024-06-12 16:28:01 -0600

    Use class syntax instead of constructor syntax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-function Todo(title) {
-  this.title = title;
-  this.description = "";
-  this.dueDate = "";
-  this.priority = 1;
-  this.done = false;
-  this.nested = [];
+class Todo {
+  constructor(title) {
+    this.title = title;
+    this.description = "";
+    this.dueDate = "";
+    this.priority = 1;
+    this.done = false;
+    this.nested = [];
+  }
+  setDescription(description) {
+    this.description = description;
+  }
+  isProject() {
+    return this.nested.length > 0;
+  }
 }
-
-Todo.prototype.setDescription = function (description) {
-  this.description = description;
-};
-
-Todo.prototype.isProject = function () {
-  return this.nested.length > 0;
-};
