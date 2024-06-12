a5ad620d9006319d9abe58065ffd1fd6de4c98fa
tg0xff <tg0xff@proton.me>
2024-06-12 15:38:21 -0600

    Add HtmlWebpackPlugin to config

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     app: "./src/index.js",
   },
+  plugins: [new HtmlWebpackPlugin()],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
