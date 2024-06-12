f6e40118d3d7cc7e30c2d484bfa94ce41f9a0c4d
tg0xff <tg0xff@proton.me>
2024-06-12 16:41:28 -0600

    Revert previous commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 class Todo {
-  #title;
-  #description = "";
-  #dueDate = "";
-  #priority = 1;
-  #done = false;
   constructor(title) {
-    this.#title = title;
+    this.title = title;
+    this.description = "";
+    this.dueDate = "";
+    this.priority = 1;
+    this.done = false;
     this.nested = [];
   }
   setDescription(description) {
