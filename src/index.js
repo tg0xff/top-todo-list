9d35bb403110ac9046f480a22c8ae3dae420a2b3
tg0xff <tg0xff@proton.me>
2024-06-12 16:18:28 -0600

    Define initial values for new properties

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 function Todo(title) {
   this.title = title;
+  this.description = "";
+  this.dueDate = "";
   this.priority = 1;
+  this.nested = [];
 }
 
 Todo.prototype.setDescription = function (description) {
